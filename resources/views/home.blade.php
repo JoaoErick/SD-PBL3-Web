1644323050f1d0f8f52913211f380d8613af2488
JoaoErick <jerick1415@outlook.com>
2021-05-05 14:50:24 -0300

    Add condition to status.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,44 @@
 
 @section('content-css')
 <link rel="stylesheet" href="{{ asset('css/home.css') }}"> 
-@livewireStyles
 @endsection
 
 @section('content')
-<livewire:show-connection />
+<form id="form" action="{{ route('verifyConnection') }}" method="post" onsubmit="return false">
+    @csrf
+</form>
+<div class="row">
+    @if ($status == 1)
+        <div class="col-md-12 d-flex justify-content-center align-items-center mt-5">
+            <i class="bi bi-cloud cloud-icon"></i>
+        </div>
+        <div class="col-md-12 d-flex justify-content-center align-items-center col-title">
+            <span class="title-cloud">Conectado</span> 
+        </div>
+    @else
+        <div class="col-md-12 d-flex justify-content-center align-items-center mt-5">
+            <i class="bi bi-cloud-slash cloud-icon"></i>
+        </div>
+        <div class="col-md-12 d-flex justify-content-center align-items-center col-title">
+            <span class="title-cloud">Desconectado</span> 
+        </div>
+    @endif
+    
+</div>
 @endsection
 
 @section('content-js')
-@livewireScripts
+<script>
+
+    function verifyConnection() {
+
+            setInterval(function () {
+                document.getElementById("form").submit();
+            }, 5000);
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            verifyConnection();
+        });
+</script>
 @endsection
