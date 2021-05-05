af69232455b6417e32bb24ae8e4d5e21088ab84f
JoaoErick <jerick1415@outlook.com>
2021-05-05 14:51:02 -0300

    Add verify connection route.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ConnectionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use App\Http\Controllers\HomeController;
 /*-------------------- Página principal --------------------------------------*/
 /*Rota para acessar a página principal */
 Route::get('/',[HomeController::class,'index'])->name('home');
+Route::post('/',[ConnectionController::class,'verifyConnection'])->name('verifyConnection');
+
