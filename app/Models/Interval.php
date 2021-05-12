ad40884fe52ca97759f2afa970ebc2df9bcfaec0
AllanCapistrano <allan.capistrano3@gmail.com>
2021-05-12 18:17:42 -0300

    Add functions.

--- app/Models/Interval.php
+++ app/Models/Interval.php
@@ -19,4 +19,32 @@ class Interval extends Model
         'created_at',
         'updated_at',
     ];
+
+    /**
+     * Função para retornar metade do intervalo que foi definido.
+     * @param string       $interval
+     */
+    public static function halfInterval($interval)
+    {
+        $str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $interval);
+
+        sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
+
+        $half_time_seconds = ($hours * 3600 + $minutes * 60 + $seconds)/2;
+
+        return gmdate("H:i:s", $half_time_seconds);
+    }
+
+    /**
+     * Função para converter o horário do formato H:m:s para milissegundos
+     * @param string       $time
+     */
+    public static function convertToMilis($time)
+    {
+        $temp = explode(":", $time);
+        
+        $time = $temp[0] * 3600000 + $temp[1] * 60000 + $temp[2] * 1000;
+
+        return $time;
+    }
 }
