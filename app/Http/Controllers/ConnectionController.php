d5ff34c9e2936a3514531648001abd8b65645030
AllanCapistrano <allan.capistrano3@gmail.com>
2021-05-12 18:22:10 -0300

    Send the new time to setInterval.

--- app/Http/Controllers/ConnectionController.php
+++ app/Http/Controllers/ConnectionController.php
@@ -19,14 +19,9 @@ class ConnectionController extends Controller
         $now = \Carbon\Carbon::now("America/Sao_Paulo");
 
         $current = strtotime($now);
-
-        $temp = explode(':', $interval->time);
-        $updated = \Carbon\Carbon::parse($connection->updated_at)->addSeconds((int) $temp[2]);
-        $updated = $updated->addMinutes((int) $temp[1]);
-        $updated = $updated->addHours((int) $temp[0]);
-        $updated = $updated->format('H:i:s');
         
-        $updated = strtotime($updated);
+        $updated = $this->updatedTime($interval->time, $connection->updated_at);
+        
         $diff = ($updated - $current);
 
         if($diff < 0){
@@ -37,6 +32,29 @@ class ConnectionController extends Controller
 
         $connection->save();
 
-        return view('home', ['status' => $connection->status]);
+        $interval = Interval::halfInterval($interval->time);
+        $interval = Interval::convertToMilis($interval);
+
+        return view('home', [
+            'status' => $connection->status,
+            'interval' => $interval
+        ]);
+    }
+
+    /**
+     * Função para calcular a última faixa de tempo que o dispositivo 
+     * estaria conectado.
+     * @param string      $intervalTime
+     * @param string      $updatedAt
+     */
+    private function updatedTime($intervalTime, $updatedAt)
+    {
+        $temp = explode(':', $intervalTime);
+        $updated = \Carbon\Carbon::parse($updatedAt)->addSeconds((int) $temp[2]);
+        $updated = $updated->addMinutes((int) $temp[1]);
+        $updated = $updated->addHours((int) $temp[0]);
+        $updated = $updated->format('H:i:s');
+
+        return strtotime($updated);
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Connection;
+use App\Models\Interval;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,14 @@ class HomeController extends Controller
     public function index()
     {
         $connection = Connection::get()->first();
-        return view('home', ['status' => $connection->status]);
+        
+        $interval = Interval::get()->first();
+        $interval = Interval::halfInterval($interval->time);
+        $interval = Interval::convertToMilis($interval);
+
+        return view('home', [
+            'status' => $connection->status,
+            'interval' => $interval
+        ]);
     }
 }
